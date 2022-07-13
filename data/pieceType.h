5d67be38dac2a2386676e8f416cbbd5abee3fdf3
t442zhan <t442zhan@uwaterloo.ca>
2022-07-13 01:50:37 -0400

    piecetype migration

---
+++ data/pieceType.h
@@ -0,0 +1,14 @@
+#ifndef PIECE_TYPE_H_
+#define PIECE_TYPE_H_
+
+enum class PieceType
+{
+    KING,
+    QUEEN,
+    ROOK,
+    BISHOP,
+    KNIGHT,
+    PAWN
+};
+
+#endif
--- moveable/moveable.h
+++ moveable/moveable.h
@@ -1,6 +1,6 @@
 #ifndef MOVEABLE_H_
 #define MOVEABLE_H_
-
+#include "../data/pieceType.h"
 #include <vector>
 
 class Board;
@@ -8,21 +8,11 @@ class Position;
 
 class Moveable
 {
-    enum class Name
-    {
-        KING,
-        QUEEN,
-        ROOK,
-        BISHOP,
-        KNIGHT,
-        PAWN
-    };
-
     virtual std::vector<Position> getDelta();
     virtual std::vector<Position> getValidMoves(Board b);
     bool isInBounds(const Board b);
 
-    Name name;
+    PieceType name;
     virtual void draw();
 
 public:
