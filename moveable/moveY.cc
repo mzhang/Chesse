2f16241f7819d887615bff1d118ccf3d5b386a17
mzhang <mcmattzhang@gmail.com>
2022-07-20 02:34:44 -0400

    moveY back

---
+++ moveable/moveY.cc
@@ -0,0 +1,71 @@
+#include <vector>
+#include <memory>
+
+#include "moveY.h"
+#include "../data/pieceType.h"
+#include "../game/gameState.h"
+#include "../data/move.h"
+#include "./piece.h"
+#include "./moveable.h"
+#include "decorator.h"
+
+using namespace std;
+
+MoveY::MoveY(unique_ptr<Moveable> component, int maxSteps) : Decorator{std::move(component)}, maxSteps{maxSteps} {}
+
+vector<Move> MoveY::getValidMoves(const GameState &g)
+{
+    vector<Move> moves = Decorator::getValidMoves(g);
+    int player = Decorator::getOwner();
+
+    Position currentPos = getPosition();
+    Position pos = currentPos;
+
+    ++pos.y;
+    while (g.isInBounds(pos) && (abs(pos.y - currentPos.y) <= maxSteps))
+    {
+        if (g.isEmpty(pos))
+        {
+            moves.push_back(Move{currentPos, pos});
+        }
+        else if (!g.isOwner(pos, player))
+        {
+            moves.push_back(Move{currentPos, pos, pos});
+            break;
+        }
+        else
+        {
+            break;
+        }
+        ++pos.y;
+    }
+
+    pos = currentPos;
+    --pos.y;
+    while (g.isInBounds(pos) && (abs(pos.y - currentPos.y) <= maxSteps))
+    {
+        if (g.isEmpty(pos))
+        {
+            moves.push_back(Move{currentPos, pos});
+        }
+        else if (!g.isOwner(pos, player))
+        {
+            moves.push_back(Move{currentPos, pos, pos});
+            break;
+        }
+        else
+        {
+            break;
+        }
+        --pos.y;
+    }
+    // moves are appended, not a union. must remove duplicates
+    return moves;
+}
+
+unique_ptr<Moveable> MoveY::clone()
+{
+    return make_unique<MoveY>(*this);
+}
+
+MoveY::MoveY(const MoveY &o) : Decorator{nullptr} {}
---
+++ moveable/moveY.h
@@ -0,0 +1,28 @@
+#ifndef MOVE_Y_H_
+#define MOVE_Y_H_
+
+#include <vector>
+#include <memory>
+
+#include "decorator.h"
+
+enum class PieceType;
+class GameState;
+class Move;
+class Piece;
+class Moveable;
+
+class MoveY : public Decorator
+{
+    int maxSteps;
+
+public:
+    MoveY(std::unique_ptr<Moveable>, int maxSteps = 1);
+
+    std::vector<Move> getValidMoves(const GameState &) override;
+    std::unique_ptr<Moveable> clone() override;
+
+    MoveY(const MoveY &);
+};
+
+#endif
