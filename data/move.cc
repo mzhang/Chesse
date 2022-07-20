83a6c6177b3661ff4cd9a08fab8610f6b12756df
mzhang <mcmattzhang@gmail.com>
2022-07-20 02:04:18 -0400

    better ctors + <<

--- data/move.cc
+++ data/move.cc
@@ -6,15 +6,13 @@ using namespace std;
 
 Move::Move() {}
 
+Move::Move(const Position &from, const Position &to) : from{from}, to{to}, capturePositions{vector<Position>{}} {}
+
 Move::Move(const vector<Position> &from, const vector<Position> &to, const vector<Position> &capture) : from{from}, to{to}, capturePositions{capture} {}
 
 Move::Move(const Position &from, const Position &to, const Position &capture) : from{vector<Position>{from}}, to{vector<Position>{to}}, capturePositions{vector<Position>{capture}} {}
 
-Move::Move(const Move &other) : from{other.from}, to{other.to}, capturePositions{other.capturePositions}
-{
-    cout << "Move(the class) copy ctor" << endl;
-    cout << "From[0]: " << from[0] << "; To[0]: " << to[0] << ";" << endl;
-}
+Move::Move(const Move &other) : from{other.from}, to{other.to}, capturePositions{other.capturePositions} {}
 
 Move::~Move() {}
 
@@ -25,6 +23,13 @@ bool Move::operator==(const Move &other) const
 
 ostream &operator<<(ostream &os, const Move &m)
 {
-    os << "From[0]: " << m.from[0] << "; To[0]: " << m.to[0] << ";" << endl;
+    for (int i = 0; i < (int)m.from.size(); i++)
+    {
+        os << m.from[i] << " to " << m.to[i];
+        if (i < (int)m.capturePositions.size())
+        {
+            os << " to capture the piece at " << m.capturePositions[i];
+        }
+    }
     return os;
 }
--- data/move.h
+++ data/move.h
@@ -10,7 +10,8 @@ struct Move
     std::vector<Position> to;
     std::vector<Position> capturePositions;
 
-    Move(const Position &, const Position &, const Position & = Position{});
+    Move(const Position &, const Position &);
+    Move(const Position &, const Position &, const Position &);
     Move(const std::vector<Position> &, const std::vector<Position> &, const std::vector<Position> & = std::vector<Position>());
     Move();
     Move(const Move &);
