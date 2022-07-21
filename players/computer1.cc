f48edbd68aa0777c8dc1087fea66fda57ff01190
ymulki <ymulki@uwaterloo.ca>
2022-07-20 23:36:15 -0400

    RNJesus

--- players/computer1.cc
+++ players/computer1.cc
@@ -1,12 +1,15 @@
 #include "computer1.h"
 #include "../game/gameState.h"
 #include "../data/move.h"
+#include "../game/board.h"
 
 Computer1::Computer1(int playerNum) : Player{playerNum}
 {
 }
 
-Move Computer1::doNextMove(const GameState &b)
+Move Computer1::doNextMove(const GameState &state)
 {
-    // TODO: implement
+    // Iterates over all pieces and all possible moves for each piece in gamestate's board
+    // randomly chooses one and returns it
+
 }
