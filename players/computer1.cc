5e55aafc08214e984f26b9d5336e24fb1fb44c60
ymulki <ymulki@uwaterloo.ca>
2022-07-20 23:43:20 -0400

    Add computer1

--- players/computer1.cc
+++ players/computer1.cc
@@ -1,3 +1,5 @@
+#include <random>
+
 #include "computer1.h"
 #include "../game/gameState.h"
 #include "../data/move.h"
@@ -12,4 +14,13 @@ Move Computer1::doNextMove(const GameState &state)
     // Iterates over all pieces and all possible moves for each piece in gamestate's board
     // randomly chooses one and returns it
 
+    auto possible_moves = state.getValidMoves(playerNum);
+
+    // use uniform distribution to select a random move from the vector of possible moves
+    std::uniform_int_distribution<int> distribution(0, possible_moves.size() - 1);
+    std::random_device rd;
+    std::mt19937 generator{rd()};
+    int random_index = distribution(generator);
+
+    return possible_moves[random_index];
 }
